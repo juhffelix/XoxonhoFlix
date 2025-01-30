2cef5fb5c21fafa52309c9f2dfc9b79ca719c537
Juliana <juhffelix@outlook.com>
2025-01-30 00:08:51 -0300

    fix: remove vulnerability

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NWJlZTJjZTczM2ZjNzZiZTZjZDA3NzIzNTRmYjMwYiIsIm5iZiI6MTczNjA5MzU2MC40MTkwMDAxLCJzdWIiOiI2NzdhYWY3OGE2ZGVmMDExMWE3NGEyODAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.cg7r0YMpVgb_z9E9m-yhyY6ygqduoW9uYVzyrzzug6Q'
+       Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`
     }
   };
   
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
+        const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1', options);
         const data = await response.json();
         setMovies(data.results); 
         console.log(data);
